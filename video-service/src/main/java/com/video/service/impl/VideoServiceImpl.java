1119473687c338809883b06ff3c9aa9d51f91aaa
CheungChingYin <CheungChingYin@outlook.com>
2018-10-25 21:30:09 +0800

    Class 2018.10.25 1.同上

---
+++ video-service/src/main/java/com/video/service/impl/VideoServiceImpl.java
@@ -0,0 +1,31 @@
+package com.video.service.impl;
+
+import com.video.mapper.VideosMapper;
+import com.video.pojo.Videos;
+import com.video.service.VideoService;
+import org.n3r.idworker.Sid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class VideoServiceImpl implements VideoService {
+
+    @Autowired
+    private VideosMapper videosMapper;
+
+    @Autowired
+    private Sid sid;
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public String saveVideo(Videos video) {
+
+        String id = sid.nextShort();
+        video.setId(id);
+        videosMapper.insertSelective(video);
+
+        return id;
+    }
+}
